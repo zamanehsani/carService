4b0838e69cd4f88177524f223a38b14c65155189
Zaman Ehsani <zamanehsani@gmail.com>
2024-02-19 21:51:55 +0400

    added api app

--- carService/urls.py
+++ carService/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls'))
+    path('', include('dashboard.urls')),
+    path('api-auth/', include('rest_framework.urls'))
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
