a863c6dc0577dafb86a3963bf9326a9d16753bd5
Zaman Ehsani <zamanehsani@gmail.com>
2024-03-10 19:59:41 +0400

    added the print credential in settings to check

--- carService/settings.py
+++ carService/settings.py
@@ -200,13 +200,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME=os.getenv("AWS_S3_REGION_NAME")
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.me-central-1.amazonaws.com'
+print("access key: ", AWS_ACCESS_KEY_ID)
+print(" secret: ", AWS_SECRET_ACCESS_KEY)
+print("bucket name: ", AWS_STORAGE_BUCKET_NAME)
+print("Region: ", AWS_S3_REGION_NAME)
+
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 STATIC_URL =  f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 
+print("Media url : ", MEDIA_URL)
+print("Static: ", STATIC_URL)
+print("Static storage: ", STATICFILES_STORAGE)
+print("Default file: ", DEFAULT_FILE_STORAGE)
 
 # Logging
 
